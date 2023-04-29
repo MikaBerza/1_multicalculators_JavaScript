c0fb88d88e6321b5c0e67d12e652f1e7a9d135d3
Mika <mikberza@mail.ru>
2023-04-29 09:24:46 +0300

    Проект 1_multicalculators_JavaScript Исправлены ошибки в файлах (passwordGeneratorScript.js) в соответствии с инструкциями ESLint
    Project 1_multicalculators_JavaScript
    Fixed file errors (passwordGeneratorScript.js), according to the instructions of ESLint

--- 6_10_passwordGeneratorPage/passwordGeneratorScript.js
+++ 6_10_passwordGeneratorPage/passwordGeneratorScript.js
@@ -1,5 +1,4 @@
-/*активация строго режима*/
-'use strict';
+// 'use strict';  отключил строгий режим по требованию eslint
 
 // Считываем кнопку сгенерировать
 const btnPasswordGenerator = document.getElementById('btnPasswordGenerator');
@@ -22,13 +21,14 @@ function getRandom(min, max) {
 function generatePassword() {
   // в переменную result накапливаем результат в цикле
   let result = '';
-  /*строка с случайными символами из которой с помощью 
+  /* строка с случайными символами из которой с помощью
     функции getRandom() в переменную result записывается пароль
-    с указанным числом символов*/
-  let str = `0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!"№;%:?*()_+`;
+    с указанным числом символов */
+  const str = `0123456789qwertyuiopasdfghjklzxcvbnmQ
+  WERTYUIOPASDFGHJKLZXCVBNM!"№;%:?*()_+`;
 
-  /* Количество случайных символов, т.е. сколько случайных символов нам 
-    нужно вывести, такое значение и задаем переменной numberOfCharacters*/
+  /* Количество случайных символов, т.е. сколько случайных символов нам
+    нужно вывести, такое значение и задаем переменной numberOfCharacters */
   const numberOfCharacters = Number(labelRangeNumber.innerHTML);
 
   for (let i = 0; i < numberOfCharacters; i++) {
@@ -41,7 +41,7 @@ function generatePassword() {
 }
 
 /*
-Метод addEventListener - добавляет обработчик события к указанному элементу и 
+Метод addEventListener - добавляет обработчик события к указанному элементу и
 запустить выполнение программы при совершении заданного действия
 */
 inputRange.addEventListener('input', setPasswordLength);
